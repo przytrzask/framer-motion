3c86f02c60b287b3102770941860156565d6bd06
Tomasz Trzaskoma <przytrzask@gmail.com>
2020-08-15 23:40:27 +0200

    animate props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React from "react";
-import { Card, CardGrid, Container, Header } from "./Elements";
-import "./App.css";
-import Menu from "./Menu";
-import blue from "./blue.png";
-import purp from "./purp.png";
-import black from "./black.png";
-import green from "./green.png";
+import React from "react"
+import { motion } from "framer-motion"
+import { Card, CardGrid, Container, Header } from "./Elements"
+import "./App.css"
+import Menu from "./Menu"
+import blue from "./blue.png"
+import purp from "./purp.png"
+import black from "./black.png"
+import green from "./green.png"
 
 function App() {
+  const [value, setValue] = React.useState(0)
+  const [opacity, setOpacity] = React.useState(1)
+  console.log(value)
   return (
-    <div>
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
       <Header>
         <Menu />
         <h1>Header</h1>
       </Header>
       <Container>
-        <h2>Super Cool</h2>
+        <motion.h2 animate={{ x: Number(value), opacity }}>
+          Super Cool
+        </motion.h2>
+        <input
+          type="range"
+          min="-100"
+          max="100"
+          onChange={(evt) => setValue(evt.currentTarget.value)}
+          value={value}
+        />
+        <button
+          onClick={() => setOpacity((prevOpacity) => (prevOpacity ? 0 : 1))}
+        >
+          Set Opacity
+        </button>
         <CardGrid>
           <Card style={{ background: "var(--purp)" }}>
             <h3>Some card</h3>
@@ -35,8 +53,8 @@ function App() {
           </Card>
         </CardGrid>
       </Container>
-    </div>
-  );
+    </motion.div>
+  )
 }
 
-export default App;
+export default App

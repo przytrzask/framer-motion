bbe8992e208fc761de14dcb4b389777c8afcec85
Tomasz Trzaskoma <przytrzask@gmail.com>
2020-08-15 23:49:48 +0200

    animate presence

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import { Card, CardGrid, Container, Header } from "./Elements"
 import "./App.css"
 import Menu from "./Menu"
@@ -10,8 +10,7 @@ import green from "./green.png"
 
 function App() {
   const [value, setValue] = React.useState(0)
-  const [opacity, setOpacity] = React.useState(1)
-  console.log(value)
+  const [show, setOpacity] = React.useState(false)
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
       <Header>
@@ -19,9 +18,19 @@ function App() {
         <h1>Header</h1>
       </Header>
       <Container>
-        <motion.h2 animate={{ x: Number(value), opacity }}>
-          Super Cool
-        </motion.h2>
+        <AnimatePresence exit="">
+          {show && (
+            <motion.h2
+              initial={{
+                opacity: 0,
+              }}
+              animate={{ opacity: 1, x: Number(value) }}
+              exit={{ opacity: 0 }}
+            >
+              Super Cool
+            </motion.h2>
+          )}
+        </AnimatePresence>
         <input
           type="range"
           min="-100"
@@ -30,7 +39,7 @@ function App() {
           value={value}
         />
         <button
-          onClick={() => setOpacity((prevOpacity) => (prevOpacity ? 0 : 1))}
+          onClick={() => setOpacity((prevShow) => (prevShow ? false : true))}
         >
           Set Opacity
         </button>
